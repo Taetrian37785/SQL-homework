c989d2169993a94de815c448d0ebba66fbb48768
unknown <tarmonkey178@aim.com>
2021-02-11 10:05:29 -0500

    First Question of homework was not saved on correct file. Copied over.

--- HW.sql
+++ HW.sql
@@ -1,4 +1,10 @@
 
+--List the following details of each employee: employee number, last name, first name, sex, and salary.
+SELECT s.salary, e.emp_no, e.first_name, e.last_name, e.sex
+FROM salaries AS s
+INNER JOIN employees AS e
+ON (s.emp_no = e.emp_no)
+;
 
 --List first name, last name, and hire date for employees who were hired in 1986.
 SELECT first_name, last_name, hire_date
